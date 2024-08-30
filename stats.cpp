7ba46d5f821500c595c91165f2910d525fa5fed0
VR, Harikrishnan <harikrishnan.vr@philips.com>
2024-08-30 17:40:29 +0530

    Build Error

--- stats.cpp
+++ stats.cpp
@@ -1,17 +1,21 @@
 #include "stats.h"
 #include <numeric>
+#include <algorithm>
 
-Statistics::Stats Statistics::ComputeStatistics(const std::vector<float>& numList)
+namespace Statistics
 {
-    Statistics::Stats stats = { NAN, NAN, NAN };
-
-    if (numList.empty())
+    Stats ComputeStatistics(const std::vector<float>& numList)
     {
+        Stats stats = { NAN, NAN, NAN };
+
+        if (numList.empty())
+        {
+            return stats;
+        }
+
+        stats.average = std::accumulate(numList.begin(), numList.end(), 0.0) / numList.size();
+        stats.max = *std::max_element(numList.begin(), numList.end());
+        stats.min = *std::min_element(numList.begin(), numList.end());
         return stats;
     }
-
-    stats.average = std::accumulate(numList.begin(), numList.end(), 0.0) / numList.size();
-    stats.max = *std::max_element(numbers.begin(), numbers.end());
-    stats.min = *std::min_element(numbers.begin(), numbers.end());
-    return stats;
 }
